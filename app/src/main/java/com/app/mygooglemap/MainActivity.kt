3dc627aeb1ef4b34043c8b8f63739665f4c82ee7
Dell <syeduzair99jamal@gmail.com>
2024-05-15 00:15:42 +0500

    Initial commit

---
+++ app/src/main/java/com/app/mygooglemap/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.app.mygooglemap
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MainActivity : AppCompatActivity(), OnMapReadyCallback {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
+
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        val loc = LatLng(24.86,67.0)
+
+        googleMap.addMarker(
+            MarkerOptions()
+                .position(loc)
+                .title("Karachi")
+        )
+    }
+}
